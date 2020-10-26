a86644cc317a4a6b1cb359c90c9783c354db6bd8
GitHubAndresDis <71302188+GitHubAndresDis@users.noreply.github.com>
2020-10-26 18:13:12 -0500

    Update clsPelotaConBorde.js

--- js/include/clsPelotaConBorde.js
+++ js/include/clsPelotaConBorde.js
@@ -1,12 +1,25 @@
 class clsPelotaConBorde {
 
     constructor(pelota) {
-        this.pelota = pelota;
+        this.id = pelota.id;
+        this.principal = pelota.principal;
+        this.color = pelota.color;
+        this.location_y = pelota.location_y;
+        this.location_x = pelota.location_x;
+        this.width = pelota.width;
+        this.height = pelota.height;
     }
 
-    DibujarBorde(color) {
+    Dibujar(control, colorBorde) {
         try {
-            $('#' + this.pelota.id).css('border', '2px solid ' + color);
+            var div = $('<div id="' + this.id + '"/>').addClass('pelota');
+            div.css('width', this.width + 'px');
+            div.css('height', this.height + 'px');
+            div.css('background-color', this.color);
+            div.css('left', this.location_x + 'px');
+            div.css('top', this.location_y + 'px');
+            div.css('border', '2px solid ' + colorBorde);
+            control.append(div);
         } catch(err){
             console.error('Error: clsDibujarPelota.Draw ' + err.toString());
         }
